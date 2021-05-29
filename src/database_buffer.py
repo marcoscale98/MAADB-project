b0d17202c60c7bda8ea044ec42a3f0c5b16cbb40
Marco Scalerandi <marcoscale98@hotmail.com>
2021-05-29 18:52:22 +0200

    completato su db il caricamento delle risorse lessicali che interessano

--- src/database_buffer.py
+++ src/database_buffer.py
@@ -6,27 +6,70 @@ from pymongo import MongoClient
 from pymongo.collection import Collection
 from pymongo.database import Database
 
+emotions = ('anger', 'anticipation', 'disgust', 'fear', 'joy', 'sadness', 'surprise', 'trust')
+
+def populate_db_lexres():
+    #inseriamo nel database `buffer_lexical_resources` una collezione per ogni emozione
+    #   per ogni parola
+    #       inserisco la parola in un documento `lemma:<parola stessa`
+    #       inserisco il nome della risorse e quante volte compare la parola stessa (1)
+    for em in emotions:
+        lemmi={}
+        emot_coll:Collection=lex_res_db[em]
+        if emot_coll.count_documents({})>0:
+            emot_coll.drop()
+        start_path = f'res/Risorse_lessicali/Archive_risorse_lessicali/{str.capitalize(em)}/'
+        end_path=f'_{em}.txt'
+        name_lex_res=('EmoSN','NRC','sentisense')
+        #trova i lemmi nelle lexical resources
+        for res in name_lex_res:
+            try:
+                path=os.path.relpath(start_path+res+end_path)
+
+                # leggiamo tutti i lemmi presenti nella risorsa lessicale
+                with open(path, 'r', encoding='utf-8') as fp:
+                    lemma = fp.readline()
+                    while (lemma):
+                        #rimuovo endline
+                        lemma=lemma.replace("\n","")
+                        #rimuovo parole composte
+                        if '_' not in lemma:
+                            l= lemmi.get(lemma,{})
+                            l[res]=1
+                            lemmi[lemma]=l
+                        lemma = fp.readline()
+            except FileNotFoundError:
+                pass
+        lemmi = list(map(lambda kv: {"lemma": kv[0], "res":kv[1]}, lemmi.items()))
+        res_insert = emot_coll.insert_many(lemmi)
+        print(f'N. documenti inseriti nella collezione {emot_coll.name}: {len(res_insert.inserted_ids)}')
+        print(f'n. documenti presenti nella collezione {emot_coll.name}: {emot_coll.count_documents({})}')
+
+
+def populate_db_twitter():
+    # inseriamo nella `backup_twitter_messages` database una collezione per ogni file txt
+    for em in emotions:
+        emot_coll: Collection = twitter_db[em]
+        if emot_coll.count_documents({}) > 0:
+            emot_coll.drop()
+        path = f'res/Twitter_messaggi/dataset_dt_{em}_60k.txt'
+        messages = []
+        path = os.path.relpath(path)
+        # leggiamo tutti i messaggi di una emozione
+        with open(path, 'r', encoding='utf-8') as fp:
+            mess = fp.readline()
+            while (mess):
+                messages.append(mess)
+                mess = fp.readline()
+        messages = list(map(lambda m: {"message": m}, messages))
+        res_insert = emot_coll.insert_many(messages)
+        print(f'N. documenti inseriti nella collezione {emot_coll.name}: {len(res_insert.inserted_ids)}')
+        print(f'n. documenti presenti nella collezione {emot_coll.name}: {emot_coll.count_documents({})}')
+
 if __name__ == '__main__':
     client=MongoClient()
     twitter_db:Database=client['buffer_twitter_messages']
     lex_res_db:Database = client['buffer_lexical_resources']
 
-    #inseriamo nella `backup_twitter_messages` database una collezione per ogni file txt
-    emotions = ('anger','anticipation','disgust','fear','joy','sadness','surprise','trust')
-    for em in emotions:
-        emot_coll:Collection=twitter_db[em]
-        if emot_coll.count_documents({})>0:
-            emot_coll.drop()
-        path=f'res/Twitter_messaggi/dataset_dt_{em}_60k.txt'
-        messages=[]
-        path=os.path.relpath(path)
-        #leggiamo tutti i messaggi di una emozione
-        with open(path,'r',encoding='utf-8') as fp:
-            mess=fp.readline()
-            while(mess):
-                messages.append(mess)
-                mess=fp.readline()
-        messages = list(map(lambda m: {"name":m},messages))
-        res_insert = emot_coll.insert_many(messages)
-        print(f'N. documenti inseriti nella collezione {emot_coll.name}: {len(res_insert.inserted_ids)}')
-        print(f'n. documenti presenti nella collezione {emot_coll.name}: {emot_coll.count_documents({})}')
+    # populate_db_twitter()
+    populate_db_lexres()
